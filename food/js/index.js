77389b64b178952aa0f09ebcc75dda7286804b16
Manthan1806 <32006356+Manthan1806@users.noreply.github.com>
2019-03-23 00:29:46 +0530

    Add files via upload

--- food/js/index.js
+++ food/js/index.js
@@ -3,6 +3,9 @@ function showMoreDesc(id) {
 }
 
 overloadCount=0;
+supremeCount=0;
+nvsupremeCount=0;
+pepperoniCount=0;
 
 function OverloadInc() {
 	overloadCount = overloadCount + 1;
@@ -12,4 +15,34 @@ function OverloadInc() {
 function OverloadDec() {
 	overloadCount = overloadCount - 1;
 	document.getElementById("over_cnt").value = overloadCount;
+}
+
+function SupremeInc() {
+	supremeCount = supremeCount + 1;
+	document.getElementById("supreme_cnt").value = supremeCount;
+}
+
+function SupremeDec() {
+	supremeCount = supremeCount - 1;
+	document.getElementById("supreme_cnt").value = supremeCount;
+}
+
+function NvSupremeInc() {
+	nvsupremeCount = nvsupremeCount + 1;
+	document.getElementById("nvsupreme_cnt").value = nvsupremeCount;
+}
+
+function NvSupremeDec() {
+	nvsupremeCount = nvsupremeCount - 1;
+	document.getElementById("nvsupreme_cnt").value = nvsupremeCount;
+}
+
+function PepperoniInc() {
+	pepperoniCount = pepperoniCount + 1;
+	document.getElementById("pepperoni_cnt").value = pepperoniCount;
+}
+
+function PepperoniDec() {
+	pepperoniCount = pepperoniCount - 1;
+	document.getElementById("pepperoni_cnt").value = pepperoniCount;
 }
